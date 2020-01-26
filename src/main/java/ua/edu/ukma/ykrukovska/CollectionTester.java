09568b43262b3f0d369dbda2bc7fcda96f8b3850
Yana Krukovska <jana.krua@gmail.com>
2020-01-26 21:58:45 +0200

    - Changed the way list of files with data is created

--- src/main/java/ua/edu/ukma/ykrukovska/CollectionTester.java
+++ src/main/java/ua/edu/ukma/ykrukovska/CollectionTester.java
@@ -3,8 +3,7 @@ package ua.edu.ukma.ykrukovska;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Arrays;
 
 public class CollectionTester {
 
@@ -13,19 +12,12 @@ public class CollectionTester {
     private static final String RESULT = "D://Studying//InfortmationRetrieval//results.txt";
 
     public static void main(String[] args) {
-        List<String> fileNames = new LinkedList<>();
-        fileNames.add(BOOK_PATH + "harrypotter1.fb2");
-        fileNames.add(BOOK_PATH + "PrideAndPrejudice.fb2");
-        fileNames.add(BOOK_PATH + "MonteCristo.fb2");
-        fileNames.add(BOOK_PATH + "DorianGray.fb2");
-        fileNames.add(BOOK_PATH + "JourneyEarth.fb2");
-        fileNames.add(BOOK_PATH + "TreasureIsland.fb2");
-        fileNames.add(BOOK_PATH + "AliceWonderland.fb2");
-        fileNames.add(BOOK_PATH + "GulliversTravels.fb2");
-        fileNames.add(BOOK_PATH + "CallOfWild.fb2");
-        fileNames.add(BOOK_PATH + "GrimmsFairyTales.fb2");
+
         CollectionBuilder collectionBuilder = new CollectionBuilder();
-        Collection collection = collectionBuilder.createCollection(fileNames);
+        Collection collection = collectionBuilder.createCollection(Arrays.asList(BOOK_PATH + "harrypotter1.fb2", BOOK_PATH + "harrypotter1.fb2",
+                BOOK_PATH + "MonteCristo.fb2", BOOK_PATH + "DorianGray.fb2", BOOK_PATH + "JourneyEarth.fb2",
+                BOOK_PATH + "TreasureIsland.fb2", BOOK_PATH + "AliceWonderland.fb2",
+                BOOK_PATH + "GulliversTravels.fb2", BOOK_PATH + "CallOfWild.fb2", BOOK_PATH + "GrimmsFairyTales.fb2"));
         System.out.println("Total size: " + collection.getTotalSizeKB() + " KB");
         System.out.println("Total word count: " + collection.getTotalWordCount());
         System.out.println("Dictionary word count: " + collection.getDictionaryWordCount());
